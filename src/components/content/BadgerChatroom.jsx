8e0f9d16abc5f9158cecc990fe851f3921d631ba
riverside234 <bobriverside24@gmail.com>
2024-02-28 18:20:06 -0800

    part 2 done

--- src/components/content/BadgerChatroom.jsx
+++ src/components/content/BadgerChatroom.jsx
@@ -1,42 +1,60 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import BadgerMessage from "./BadgerMessage";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function BadgerChatroom(props) {
+  const [messages, setMessages] = useState([]);
 
-    const [messages, setMessages] = useState([]);
+  const loadMessages = () => {
+    fetch(
+      `https://cs571.org/api/f23/hw6/messages?chatroom=${props.name}&page=1`,
+      {
+        headers: {
+          "X-CS571-ID": CS571.getBadgerId(),
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setMessages(json.messages);
+      });
+  };
 
-    const loadMessages = () => {
-        fetch(`https://cs571.org/api/f23/hw6/messages?chatroom=${props.name}&page=1`, {
-            headers: {
-                "X-CS571-ID": CS571.getBadgerId()
-            }
-        }).then(res => res.json()).then(json => {
-            setMessages(json.messages)
-        })
-    };
+  console.log(messages);
+  // Why can't we just say []?
+  // The BadgerChatroom doesn't unload/reload when switching
+  // chatrooms, only its props change! Try it yourself.
+  useEffect(loadMessages, [props]);
 
-
-    // Why can't we just say []?
-    // The BadgerChatroom doesn't unload/reload when switching
-    // chatrooms, only its props change! Try it yourself.
-    useEffect(loadMessages, [props]);
-
-    return <>
-        <h1>{props.name} Chatroom</h1>
-        {
-            /* TODO: Allow an authenticated user to create a post. */
-        }
-        <hr/>
-        {
-            messages.length > 0 ?
-                <>
-                    {
-                        /* TODO: Complete displaying of messages. */
-                    }
-                </>
-                :
-                <>
-                    <p>There are no messages on this page yet!</p>
-                </>
-        }
+  return (
+    <>
+      <h1>{props.name} Chatroom</h1>
+      {/* TODO: Allow an authenticated user to create a post. */}
+      <hr />
+      {messages.length > 0 ? (
+        <>
+          <Container fluid>
+            <Row>
+              {messages.map((message) => {
+                return (
+                  <Col key={message.id} xs={12} sm={12} md={6} lg={4} xl={3}>
+                    <BadgerMessage
+                      title={message.title}
+                      poster={message.poster}
+                      content={message.content}
+                      created={message.created}
+                    />
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </>
+      ) : (
+        <>
+          <p>There are no messages on this page yet!</p>
+        </>
+      )}
     </>
+  );
 }
